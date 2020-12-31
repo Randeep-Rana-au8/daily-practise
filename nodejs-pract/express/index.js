0718ff95c6b28da6496fcc98fd997d68aab86b75
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2020-12-31 19:23:38 +0530

    added more routes to the express app

--- nodejs-pract/express/index.js
+++ nodejs-pract/express/index.js
@@ -4,4 +4,21 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Health Check ok");
 });
+
+app.get("/api", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      name: "Randeep Rana",
+    },
+    {
+      id: 2,
+      name: "Suraj Rana",
+    },
+    {
+      id: 3,
+      name: "Deepanshu Rana",
+    },
+  ]);
+});
 app.listen(4000, () => console.log("App is running on port 4000"));
