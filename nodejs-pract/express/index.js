49f6ce9eb53bd3a00080e55423b99b962b0da4ec
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-01 14:12:18 +0530

    added some data throught post request

--- nodejs-pract/express/index.js
+++ nodejs-pract/express/index.js
@@ -1,24 +1,38 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
+const datas = [
+  {
+    id: 1,
+    name: "Randeep Rana",
+  },
+  {
+    id: 2,
+    name: "Suraj Rana",
+  },
+  {
+    id: 3,
+    name: "Deepanshu Rana",
+  },
+];
 
 app.get("/", (req, res) => {
   res.send("Health Check ok");
 });
 
-app.get("/api", (req, res) => {
-  res.send([
-    {
-      id: 1,
-      name: "Randeep Rana",
-    },
-    {
-      id: 2,
-      name: "Suraj Rana",
-    },
-    {
-      id: 3,
-      name: "Deepanshu Rana",
-    },
-  ]);
+app.get("/users", (req, res) => {
+  res.send(datas);
+});
+
+app.post("/users", (req, res) => {
+  const newUser = { id: datas.length + 1, name: req.body.name };
+  datas.push(newUser);
+  res.send("Data Added");
+});
+
+app.get("/user/:id", (req, res) => {
+  const user = datas.find((data) => data.id === parseInt(req.params.id));
+  if (!user) res.status(404).send("The user is not available for this id");
+  res.send(user);
 });
 app.listen(4000, () => console.log("App is running on port 4000"));
