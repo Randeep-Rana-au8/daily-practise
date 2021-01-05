07da6f5575fb6cedf83458874bdec04ae240bb7d
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-05 22:52:54 +0530

    added filter functionality through regular expressions

--- nodejs-pract/mongodb-pract/index.js
+++ nodejs-pract/mongodb-pract/index.js
@@ -38,11 +38,79 @@ async function getCourses() {
 */
 // getCourses();
 
+//THIS FUNCTION IS RETUNING FILTERED VALUES
+// async function getCourseByFiltering() {
+//   const res = await Course.find({ isReady: true })
+//     .sort({ name: 1 })
+//     .limit(10)
+//     .select({
+//       name: 1,
+//       author: 1,
+//     });
+//   console.log(res);
+// }
+// getCourseByFiltering();
+
+//THIS FUNCTION IS RETURNING FILTERED VALUES WITH COMPARISION QUERY OPERATORS
+// async function getCourseByFiltering() {
+//eq => equals to
+//neq => not equals to
+//lt => less than
+//lte => less than or equals to
+//gt => greator than
+//gte => greator than or equals to
+//in => returns the element if the values match the values of the array
+//nin => not in
+
+//   const res = await Course
+//     .find({ $in: [10, 20, 25] })
+//     .sort({ name: 1 })
+//     .limit(10)
+//     .select({
+//       name: 1,
+//       author: 1,
+//     });
+//   console.log(res);
+// }
+// getCourseByFiltering();
+
+//  THIS FUNCTION RETURNING FILTERED VALUE BY LOGICAL OPERATORS
+// async function getCourseByFiltering() {
+// logical operators
+// or =>
+// and =>
+//   const res = await Course.find({})
+//     .or([{ isReady: true }, { author: "Randeep Rana" }])
+//     .sort({ name: 1 })
+//     .limit(10)
+//     .select({
+//       name: 1,
+//       author: 1,
+//     });
+//   console.log(res);
+// }
+// getCourseByFiltering();
+
+// THIS FUNCTION RETURNS FILTERED VALUE BY REGULAR EXPRESSIONS
 async function getCourseByFiltering() {
-  const res = await Course.find({ isReady: true }).select({
-    name: 1,
-    author: 1,
-  });
+  // REGULAR EXPRESSIONS
+
+  const res = await Course
+    // if i want to console the elements in which author name starts with Randeep
+    .find({ name: /^Randeep/ })
+
+    //if i want to console the elements in which author name ends with Rana
+    .find({ name: /Rana$/ })
+
+    //if i want to console the elements in which author name contains Rana
+    .find({ name: /.*Rana.*/ })
+
+    .sort({ name: 1 })
+    .limit(10)
+    .select({
+      name: 1,
+      author: 1,
+    });
   console.log(res);
 }
 getCourseByFiltering();
