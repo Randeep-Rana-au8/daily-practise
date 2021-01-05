8b5c9ef4e4c9c145d9883f379564ae5944873d0d
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-05 22:09:19 +0530

    added getCourseByFiltering function which returns filtered result

--- nodejs-pract/mongodb-pract/index.js
+++ nodejs-pract/mongodb-pract/index.js
@@ -17,22 +17,32 @@ const schema = mongoose.Schema({
 const Course = mongoose.model("Course", schema);
 
 //this is the function to create new documents in "courses" collection
-async function createCourse() {
-  const course = new Course({
-    name: "Reactjs course",
-    author: "Randeep Rana",
-    isReady: true,
-  });
+// async function createCourse() {
+//   const course = new Course({
+//     name: "Driving course",
+//     author: "Randeep Rana",
+//     isReady: false,
+//   });
 
-  const result = await course.save();
-  console.log(result);
-}
-createCourse();
+//   const result = await course.save();
+//   console.log(result);
+// }
+// createCourse();
 
 //this is the function which returns the all the items from a collection
+/* THIS IS THE METHOD TO GET ALL DATA
 async function getCourses() {
   const res = await Course.find({});
   console.log(res);
 }
+*/
+// getCourses();
 
-getCourses();
+async function getCourseByFiltering() {
+  const res = await Course.find({ isReady: true }).select({
+    name: 1,
+    author: 1,
+  });
+  console.log(res);
+}
+getCourseByFiltering();
