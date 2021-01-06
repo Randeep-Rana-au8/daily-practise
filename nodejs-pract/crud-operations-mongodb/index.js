e1028363ce20452ff6a8be5efd9636e635ccb6bc
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-06 21:51:35 +0530

    mongodb update operators practised

--- nodejs-pract/crud-operations-mongodb/index.js
+++ nodejs-pract/crud-operations-mongodb/index.js
@@ -13,13 +13,44 @@ const schema = {
 };
 
 const Course = mongoose.model("Course", schema);
+// Method 1 TO UPDATE A FIELD
+// async function updateCourse(id) {
+//   const course = await Course.findById(id);
+//   course.name = "HTML CSS JAVASCRIPT ALL IN ONE";
+//   course.author = "Randeep Rana";
+//   const result = await course.save();
+//   console.log(result);
+// }
+// updateCourse("5a68fdd7bee8ea64649c2777");
 
-async function getCourse(id) {
-  const course = await Course.findById(id);
-  course.name = "HTML CSS JAVASCRIPT ALL IN ONE";
-  course.author = "Randeep Rana";
-  const result = await course.save();
-  console.log(result);
+// METHOD 2 TO UPDATE A FIELD
+// async function updateCourse(id) {
+//   const course = await Course.updateOne(
+//     { _id: id },
+//     {
+//       $set: {
+//         name: "Driving",
+//         author: "Dilip Joshi",
+//       },
+//     }
+//   );
+//   console.log(course);
+// }
+// updateCourse("5a68fdd7bee8ea64649c2777");
+
+// METHOD 2 TO UPDATE A FIELD
+async function updateCourse(id) {
+  const course = await Course.findByIdAndUpdate(
+    id,
+    {
+      $set: {
+        name: "Cooking",
+        author: "apna jayka",
+      },
+    },
+    { new: true }
+  );
+
+  console.log(course);
 }
-
-getCourse("5a68fdd7bee8ea64649c2777");
+updateCourse("5a68fdd7bee8ea64649c2777");
