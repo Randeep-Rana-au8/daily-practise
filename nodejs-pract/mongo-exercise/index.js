2a27d6fd0efd10a7bfb819824bcf8928867f5195
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-06 17:41:34 +0530

    mongodb practise exercise 2

--- nodejs-pract/mongo-exercise/index.js
+++ nodejs-pract/mongo-exercise/index.js
@@ -16,16 +16,28 @@ const schema = new mongoose.Schema({
 
 const Course = mongoose.model("Course", schema);
 
-async function createCourse() {
-  const result = await Course.find({ isPublished: true, tags: "backend" })
-    .sort({ name: 1 })
-    .select({ author: 1, name: 1 });
-  return result;
+// EXERCISE 1
+// async function createCourse() {
+//   const result = await Course.find({ isPublished: true, tags: "backend" })
+//     .sort({ name: 1 })
+//     .select({ author: 1, name: 1 });
+//   return result;
+// }
+
+// async function run() {
+//   const output = await createCourse();
+//   console.log(output);
+// }
+// createCourse();
+// run();
+
+// EXERCISE 2
+async function getCourse() {
+  const courses = await Course.find({ isPublished: true })
+    .or([{ tags: "backend" }, { tags: "frontend" }])
+    .sort({ price: -1 })
+    .select({ name: 1, author: 1 });
+  console.log(courses);
 }
 
-async function run() {
-  const output = await createCourse();
-  console.log(output);
-}
-createCourse();
-run();
+getCourse();
