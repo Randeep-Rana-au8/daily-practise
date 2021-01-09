44c2ebef01774595fa2ee2694ba3bfbe849a67e3
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-09 22:02:40 +0530

    learned and practised embedding method in mongo

---
+++ nodejs-pract/embedding-documents/embedding.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String,
+});
+
+const Author = mongoose.model("Author", authorSchema);
+
+const Course = mongoose.model(
+  "Course",
+  new mongoose.Schema({
+    name: String,
+    author: {
+      type: authorSchema,
+      required: true,
+    },
+  })
+);
+
+async function createCourse(name, author) {
+  const course = new Course({
+    name,
+    author,
+  });
+
+  const result = await course.save();
+  console.log(result);
+}
+
+async function listCourses() {
+  const courses = await Course.find();
+  console.log(courses);
+}
+
+createCourse("Node Course", new Author({ name: "Mosh" }));
