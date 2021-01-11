e931469e8dfba1c554142b0d5609aaad5e16e9b6
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-11 15:01:23 +0530

    added rentals collection in mongodb

--- nodejs-pract/vidly/index.js
+++ nodejs-pract/vidly/index.js
@@ -1,53 +1,22 @@
-const express = require("express");
-const Joi = require("joi");
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const genres = require('./routes/genres');
+const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
+const express = require('express');
 const app = express();
+
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 app.use(express.json());
+app.use('/api/genres', genres);
+app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
-const collection = [
-  { id: 1, Genre: "Shoes" },
-  { id: 2, Genre: "Bags" },
-  { id: 3, Genre: "Jeans" },
-];
-
-app.get("/", (req, res) => {
-  res.send("Health Check ok");
-});
-
-app.get("/api/genre", (req, res) => {
-  res.send(collection);
-});
-
-app.get("/api/genre/:id", (req, res) => {
-  const genre = collection.find((data) => data.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("Data is not present for this id");
-  res.send(genre);
-});
-
-app.post("/api/genre/collection", (req, res) => {
-  const { error } = inpValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-  const newData = { id: collection.length + 1, name: req.body.name };
-  collection.push(newData);
-  res.send("Data Added");
-});
-
-app.put("/api/genre/:id", (req, res) => {
-  const genre = collection.find((data) => data.id === parseInt(req.params.id));
-  if (!genre) return res.status(404).send("Data is not present for this id");
-  //   const { error } = inpValidation(req.body);
-  //   if (error) return res.status(400).send(error.details[0].message);
-  genre.Genre = req.body.Genre;
-  res.send(genre);
-});
-
-app.listen(4000, (err) => {
-  if (err) console.log(err);
-  console.log("App is running on port 4000");
-});
-
-function inpValidation(data) {
-  const schema = {
-    name: Joi.string().min(3).required(),
-  };
-  return Joi.validate(data, schema);
-}
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
