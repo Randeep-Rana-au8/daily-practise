f4d132773883514a2c0a6383fda8302ef22b163d
Randeep-Rana-au8 <randeepsinghchauhan23@gmail.com>
2021-01-11 17:21:09 +0530

    added object with lodash

--- nodejs-pract/vidly/models/register.js
+++ nodejs-pract/vidly/models/register.js
@@ -14,7 +14,7 @@ const User = mongoose.model("User", userSchema);
 function validateRegister(user) {
   const schema = {
     name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(50).required().email(),
     password: Joi.string().min(5).required(),
   };
 
--- nodejs-pract/vidly/routes/register.js
+++ nodejs-pract/vidly/routes/register.js
@@ -1,17 +1,24 @@
 const { User, validate } = require("../models/register");
 const express = require("express");
 const app = express();
-const Joi = require("joi");
+const _ = require("lodash");
 
 app.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already exists!!");
+
+  //Normal object
+  //   user = new User({
+  //     name: req.body.name,
+  //     email: req.body.email,
+  //     password: req.body.password,
+  //   });
+
+  // object with lodash
+  user = new User(_.pick(req.body, ["name", " email", "password"]));
 
   await user.save();
   res.send("User is registered");
